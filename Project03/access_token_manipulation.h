d630068dce1606f00ea966f8445a499485b28aae
nila27 <9asdsxc9@nvaer.com>
2023-04-10 20:24:32 +0900

    test2

--- Project03/access_token_manipulation.h
+++ Project03/access_token_manipulation.h
@@ -6,7 +6,7 @@
 namespace win {
 
 
-	class DataEncryptedforImpact {
+	class AccessTokenManipulation {
 	public:
 		DWORD ParentPidSpoofing(DWORD parent_proc_infod, std::wstring_view process_path);
 		DWORD TokenImpersonationTheft(DWORD target_pid, std::wstring_view create_process_name);
